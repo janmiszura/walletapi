d28d707bd48a5b266ba3f211bded2d023a31f01e
Jan Miszura <janmiszura@gmail.com>
2021-04-06 11:38:41 -0300

    Configurado Spring Data, Lombok, criado entity User e UserRepository

---
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.wallet.repository;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wallet.entity.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+	
+	@Autowired
+	UserRepository repository;
+	
+	@Test
+	public void testSave() {
+		User u = new User();
+		u.setName("Teste");
+		u.setPassword("123");
+		u.setEmail("email@teste.com");
+		
+		User response = repository.save(u);
+		
+		assertNotNull(response);
+	}
+}
