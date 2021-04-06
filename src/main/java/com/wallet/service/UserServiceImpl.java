feff53a41893e6da4a90bc855d6fe2600b50ae9a
Jan Miszura <janmiszura@gmail.com>
2021-04-06 14:10:20 -0300

    UserService: criado

---
+++ src/main/java/com/wallet/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wallet.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallet.entity.User;
+import com.wallet.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	public Optional<User> findByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+	
+}
