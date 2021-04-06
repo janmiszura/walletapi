1f859de7bed9d1decbabc25e5d09182fda7e8430
Jan Miszura <janmiszura@gmail.com>
2021-04-06 11:57:37 -0300

    UserRepository: criado findByEmail

--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.wallet.repository;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +20,29 @@ import com.wallet.entity.User;
 @SpringBootTest
 public class UserRepositoryTest {
 	
+	private static String EMAIL = "email@teste.com.br";
+	
 	@Autowired
 	UserRepository repository;
+	
+	@BeforeEach
+	public void setUp() {
+		
+		User u = new User();
+		u.setName("Teste");
+		u.setPassword("123");
+		u.setEmail(EMAIL);
+		
+		repository.save(u);
+		
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		
+		repository.deleteAll();
+		
+	}
 	
 	@Test
 	public void testSave() {
@@ -27,5 +54,16 @@ public class UserRepositoryTest {
 		User response = repository.save(u);
 		
 		assertNotNull(response);
+	}
+	
+	@Test
+	public void testFindByEmail() {
+		
+		Optional<User> user = repository.findByEmail(EMAIL);
+		
+		assertTrue(user.isPresent());
+		
+		assertEquals(EMAIL, user.get().getEmail());
+		
 	}
 }
